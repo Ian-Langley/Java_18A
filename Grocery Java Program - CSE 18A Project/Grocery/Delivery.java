8198e3f1c268b7f7947123448560697d9108d489
Ian-Langley <60118887+Ian-Langley@users.noreply.github.com>
2021-02-11 12:26:21 -0800

    Verification Delivery implemented
    Verification  for day Delivery implemented

--- Grocery Java Program - CSE 18A Project/Grocery/Delivery.java
+++ Grocery Java Program - CSE 18A Project/Grocery/Delivery.java
@@ -5,11 +5,26 @@ class Delivery{
   String day;
   String time;
   Scanner usrIn = new Scanner(System.in);
-
+  //Gets the day for delivery with infinite loop until correct option chosen.
   void getDayForDeliver(){
-    System.out.println("Please input the day of the week you wish to have your groceries deliverd.");
-    day = userIn.nextLine();
+    boolean verified = false;
+    while(!verified){
+      System.out.println("Please input the day of the week you wish to have your groceries deliverd.");
+      System.out.println("Monday, Tuesday, Wednesday, Thursday, Friday, Sat, Sunday");
+      switch(userIn.nextLine()){
+        case "Monday": day = "Monday"; verified = true; break;
+        case "Tuesday": day = "Tuesday"; verified = true; break;
+        case "Wednesday": day = "Wednesday"; verified = true; break;
+        case "Thursday": day = "Thursday"; verified = true; break;
+        case "Friday": day = "Friday"; verified = true; break;
+        case "Saturday": day = "Saturday"; verified = true; break;
+        case "Sunday": day = "Sunday"; verified = true; break;
+        default:
+          System.out.println("The Day you entered does not match our options.");
+      }
+    }
   }
+  //Gets the time for delivery with infinite loop until correct option chosen.
   void getTimeForDeliver(){
     boolean verified = false;
     while(!verified){
